87ba18e497c179575ea5d5d91976af0349e718ec
clarissamanurungg <clarissamanurung0807@gmail.com>
2024-01-15 11:16:16 +0700

    12S23049_Clarissa Manurung

--- t00_02.c
+++ t00_02.c
@@ -1,9 +1,10 @@
-// NIM - NAMA
+// 12S23049 - Clarissa Manurung
 
 #include <stdio.h>
 
 int main(char **_argc, int _argv)
 {
-    // Show your code here ...
+    printf("Aloha!");
+
     return 0;
 }
